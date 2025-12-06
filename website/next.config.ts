21913f6e3980dc996c088e6abe55455db4b0a08a
Fractiunate <fractiunate@gmail.com>
2025-12-06 13:04:29 +0100

    fix: add trailingSlash for correct site export

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
-  basePath: "/client-tools"
+  basePath: "/client-tools",
+  trailingSlash: true,
 };
 
 export default nextConfig;
