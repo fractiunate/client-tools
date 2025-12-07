3a078d0262adcfc0d314e66104e541583e31b72e
Fractiunate <fractiunate@gmail.com>
2025-12-07 13:51:17 +0100

    fix: add build

--- website/dist/dev/server/chunks/ssr/_next-internal_server_app_cert-generator_page_actions_9513771a.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/cert-generator/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_cert-generator_page_actions_9513771a.js.map
