d6ccd550c3ba047a8c8bfc8af26c8e043e4cc86b
Fractiunate <fractiunate@gmail.com>
2025-12-08 14:43:57 +0100

    feat: tmp enable all features

--- website/dist/dev/server/chunks/ssr/_next-internal_server_app_json-formatter_page_actions_295a009f.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/json-formatter/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_json-formatter_page_actions_295a009f.js.map
--- website/dist/dev/static/chunks/app_json-formatter_page_tsx_7cfce4c4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_abe42287._.js",
-  "static/chunks/node_modules_9e1b9329._.js"
-],
-    source: "dynamic"
-});
